2fbed494f732a2ebe85281c9a1abe3d2ad173c63
lmedini <lionel.medini@univ-lyon1.fr>
2018-09-12 10:38:45 +0200

    Added extra field in Film.

--- metier-base/src/main/java/fr/univlyon1/m2tiw/tiw1/metier/Film.java
+++ metier-base/src/main/java/fr/univlyon1/m2tiw/tiw1/metier/Film.java
@@ -2,11 +2,15 @@ package fr.univlyon1.m2tiw.tiw1.metier;
 
 public class Film {
     private final String titre;
+    //Langue du film (VO, VF...)
     private final String version;
+    //La fiche du film sur Linked Movie Database
+    private final String fiche;
 
-    public Film(String titre, String version) {
+    public Film(String titre, String version, String fiche) {
         this.titre = titre;
         this.version = version;
+        this.fiche = fiche;
     }
 
     public String getTitre() {
@@ -15,5 +19,9 @@ public class Film {
 
     public String getVersion() {
         return version;
+    }
+
+    public String getFiche() {
+        return fiche;
     }
 }
