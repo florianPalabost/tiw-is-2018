308990d9e6382935b78664e6d838d6dd647affbd
lmedini <lionel.medini@univ-lyon1.fr>
2018-09-18 17:08:33 +0200

    Correction du test.

--- metier-base/src/test/java/fr/univlyon1/m2tiw/tiw1/test/TestAllSeances.java
+++ metier-base/src/test/java/fr/univlyon1/m2tiw/tiw1/test/TestAllSeances.java
@@ -3,6 +3,8 @@ package fr.univlyon1.m2tiw.tiw1.test;
 import fr.univlyon1.m2tiw.tiw1.metier.Cinema;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class TestAllSeances {
     //TODO importer le fichier mon-cinema.json
     private final Cinema cinema = new Cinema("Mon Cinema");
@@ -11,7 +13,7 @@ public class TestAllSeances {
     /*
      * Teste si on a bien 4 séances / jour x 7 jours x 3 salles = 84 séances
      */
-    public void test() {
-        assert cinema.getNbSeances() == 84;
+    public void testSeances() {
+        assertEquals(84, cinema.getNbSeances());
     }
 }
