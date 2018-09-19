7a1f31fc70f7c4cd1f17a3661b64ceb43a754dac
lmedini <lionel.medini@univ-lyon1.fr>
2018-09-19 10:00:53 +0200

    Deplacement test

--- metier-base/src/test/java/fr/univlyon1/m2tiw/tiw1/metier/CinemaTest.java
+++ metier-base/src/test/java/fr/univlyon1/m2tiw/tiw1/metier/CinemaTest.java
@@ -1,19 +1,19 @@
-package fr.univlyon1.m2tiw.tiw1.test;
+package fr.univlyon1.m2tiw.tiw1.metier;
 
-import fr.univlyon1.m2tiw.tiw1.metier.Cinema;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.*;
 
-public class TestAllSeances {
+public class CinemaTest {
     //TODO importer le fichier mon-cinema.json
     private final Cinema cinema = new Cinema("Mon Cinema");
 
     @Test
-    /*
+    /**
      * Teste si on a bien 4 séances / jour x 7 jours x 3 salles = 84 séances
      */
-    public void testSeances() {
+    public void getNbSeances() throws Exception {
         assertEquals(84, cinema.getNbSeances());
     }
 }
