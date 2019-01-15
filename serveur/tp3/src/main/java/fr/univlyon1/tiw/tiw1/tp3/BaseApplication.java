d526249232d908a9b9a9afed25bfecd7cf6b4e10
florian <florian.palabost@etu.univ-lyon1.fr>
2019-01-15 17:57:16 +0100

    multimodule marche mais generation des films.json et seances.json a voir

--- serveur/tp3/src/main/java/fr/univlyon1/tiw/tiw1/tp3/BaseApplication.java
+++ serveur/tp3/src/main/java/fr/univlyon1/tiw/tiw1/tp3/BaseApplication.java
@@ -2,10 +2,14 @@ package fr.univlyon1.tiw.tiw1.tp3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "fr.univlyon1.tiw.tiw1" })
+@EnableJpaRepositories(basePackages = {"fr.univlyon1.tiw"})
+@EntityScan(basePackages = {"fr.univlyon1.tiw"})
+@ComponentScan(basePackages = { "fr.univlyon1.tiw" })
 public class BaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class, args);
